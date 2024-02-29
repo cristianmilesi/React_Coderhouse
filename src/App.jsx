0d2b2728cd381124a7085090d382ba9742a0d32e
cristianmilesi <cristianmilesi.dev@gmail.com>
2024-02-29 15:38:59 -0300

    Agregado de Layout y enrutamiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ItemCountContainer from "./components/common/ItemCount/ItemCountContainer";
-import { Navbar } from "./components/layout";
-import { Footer } from "./components/layout/Footer/Footer";
 import { ItemListContainer } from "./components/pages";
 import { ItemDetailContainer } from "./components/pages/ItemDetailContainer/ItemDetailContainer";
 import { Cart, ProductCard } from "./components/common";
+import { Layout } from "./components/layout/Layout";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
         <Routes>
-          <Route path="/" element={<ItemListContainer />} />
-          <Route path="/category/:category" element={<ItemListContainer />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/item/:id" element={<ItemDetailContainer />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<ItemListContainer />} />
+            <Route path="/category/:category" element={<ItemListContainer />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/item/:id" element={<ItemDetailContainer />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,7 +1,8 @@
-
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
-
+    <>
+      <h3>MusicMart</h3>
+      <p>Tu tienda de instrumentos musicales</p>
+    </>
+  );
+};
